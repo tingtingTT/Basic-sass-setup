2bb038870b31e8435d2e242300800bdc6436f741
Tingting Wang <twang80@ucsc.edu>
2018-02-10 14:52:52 -0800

    draft final

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "350px";
+    document.getElementById("main").style.marginLeft = "350px";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function showAll(){
+    console.log("clicked");
+    document.getElementById("listItems").style.display = "block";
+    $("input").removeClass("invisible");
+    $("input").siblings().removeClass("invisible");
+}
+
+function showComplete(){
+    console.log("clicked");
+    $("input").removeClass("invisible");
+    $("input").siblings().removeClass("invisible");
+    $("input:not(:checked)").addClass("invisible");
+    $("input:not(:checked)").siblings().addClass("invisible");
+}
+
+
+function showTodo(){
+    console.log("clicked");
+    $("input").removeClass("invisible");
+    $("input").siblings().removeClass("invisible");
+    $("input:checked").addClass("invisible");
+    $("input:checked").siblings().addClass("invisible");
+}
